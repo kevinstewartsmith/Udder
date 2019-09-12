0d559a3d8bb5c55267ecb70f921eb37b19561631
kssmith9 <kssmith9@ncsu.edu>
2019-09-12 13:59:43 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Kevin Smith on 9/12/19.
+//  Copyright © 2019 Kevin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
