558e779c2b3688e1dbfeeb22f1fa7ea1b601e757
kssmith9 <kssmith9@ncsu.edu>
2019-09-12 14:52:39 +0700

    Add viewDidIfLoad

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -16,7 +16,9 @@ class ViewController: UIViewController {
     }
 
     override func viewDidDisappear(_ animated: Bool) {
-        <#code#>   
+        <#code#>
     }
+    override var viewIfLoaded: UIView?
 }
 
+   
